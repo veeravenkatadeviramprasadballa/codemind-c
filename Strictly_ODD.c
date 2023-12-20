9c2391ae2453c91940e6fa5753e311d759ba9659
veeravenkatadeviramprasadballa <152369525+veeravenkatadeviramprasadballa@users.noreply.github.com>
2023-12-20 10:44:57 +0530

    #include<stdio.h> int main() { 	int i,a; 	scanf("%d",&a); 	int arr[a]; 	for(i=0;i<a;i++) 	{ 		scanf("%d",&arr[i]); 	} 	int count=0; 	for(i=0;i<a;i++) 	{ 		if(arr[i]%2==0&&i%2==0) 		count++; 	} 	if(count>=a/2) printf("True"); 	else printf("False"); }

---
+++ Strictly_ODD.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i,a;
+	scanf("%d",&a);
+	int arr[a];
+	for(i=0;i<a;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	int count=0;
+	for(i=0;i<a;i++)
+	{
+		if(arr[i]%2==0&&i%2==0)
+		count++;
+	}
+	if(count>=a/2) printf("True");
+	else printf("False");
+}
+
